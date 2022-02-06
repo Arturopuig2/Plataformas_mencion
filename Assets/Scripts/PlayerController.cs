7e7c3e7ffdd793aa7024eef45cf52bb5f988fbfa
Arturo Puig <arturopuig2@gmail.com>
2022-02-06 13:30:25 +0100

    ejercicio 4 y 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public int speed;
+    public int force;
+    //float movement;
+    Rigidbody2D playerRB;
+    SpriteRenderer direccion;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
+        direccion=GetComponent<SpriteRenderer>();
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        float movement = Input.GetAxis("Horizontal");
+
+        if (movement < 0){
+           // direccion.flipX = enabled;
+            direccion.flipX=true;
+
+
+        }
+        else if(movement > 0)
+        {
+            //direccion.flipX = !enabled;
+            direccion.flipX = false;
+
+        }
+
+        playerRB.velocity = new Vector2(movement * force, playerRB.velocity.y);
+
+     //   playerRB.velocity = Vector2.right *movement* speed;
+
     }
 
     // Update is called once per frame
