b2a4c9bd296baac92e57d61ecb6717184081ee7f
Arturo Puig <arturopuig2@gmail.com>
2022-02-18 18:48:15 +0100

    ejercicio 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,44 +11,31 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D playerRB;
     SpriteRenderer direccion;
 
-
-
-
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
         direccion=GetComponent<SpriteRenderer>();
-
     }
-
-
     private void FixedUpdate()
     {
         float movement = Input.GetAxis("Horizontal");
-
         if (movement < 0){
            // direccion.flipX = enabled;
             direccion.flipX=true;
-
-
         }
-        else if(movement > 0)
+        else if (movement > 0)
         {
             //direccion.flipX = !enabled;
             direccion.flipX = false;
-
         }
 
         playerRB.velocity = new Vector2(movement * force, playerRB.velocity.y);
-
      //   playerRB.velocity = Vector2.right *movement* speed;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
