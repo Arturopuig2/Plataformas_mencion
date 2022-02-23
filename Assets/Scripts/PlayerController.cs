2d7608c5e8a3025069d59181b593ccceb9efd209
Arturo Puig <arturopuig2@gmail.com>
2022-02-23 02:18:36 +0100

    ejercicio 10

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public int speed;
-    public int force;
-    float movement;
+    public int speed, force;
+    float movimientoHorizontal, movimientoVertical;
     Rigidbody2D playerRB;
     SpriteRenderer direccion;
     Animator MyAnim;
-
+    Vector2 movimiento;
+    public float jumpForce;
+    bool isGrounded = true;
+    RaycastHit2D hit;
 
 
     // Start is called before the first frame update
@@ -20,26 +21,72 @@ public class PlayerController : MonoBehaviour
         playerRB = GetComponent<Rigidbody2D>();
         direccion=GetComponent<SpriteRenderer>();
         MyAnim = GetComponent<Animator>();
+        Physics2D.queriesStartInColliders = false;  //Para evitar que se detecte a sí mismo.
     }
     private void FixedUpdate()
     {
-        movement = Input.GetAxis("Horizontal");
-        if (movement < 0){
-           // direccion.flipX = enabled;
-            direccion.flipX=true;
-        }
-        else if (movement > 0)
-        {
-            //direccion.flipX = !enabled;
+
+        MyAnim.SetFloat("VerticalVelocity", playerRB.velocity.y);
+        Debug.Log("Velocidad en y " + playerRB.velocity.y);
+
+        movimientoHorizontal = Input.GetAxisRaw("Horizontal");       
+        if (movimientoHorizontal < 0){
+            direccion.flipX = true;
+            }else if (movimientoHorizontal > 0){
             direccion.flipX = false;
+            }
+
+        movimientoVertical = Input.GetAxisRaw("Jump");
+        if (movimientoVertical > 0 && isGrounded == true)
+        {
+            playerRB.velocity=new Vector2(playerRB.velocity.x, 0); //neutralizar la fuerza que llevase.
+            playerRB.AddForce(new Vector2(0, jumpForce),ForceMode2D.Impulse);
+
+                isGrounded = false;
+                MyAnim.SetBool("IsGrounded", false);
+
         }
 
-        playerRB.velocity = new Vector2(movement * force, playerRB.velocity.y);
-        //   playerRB.velocity = Vector2.right *movement* speed;
 
-        Debug.Log(movement);
+        hit = Physics2D.Raycast(transform.position, -Vector2.up);
 
-        MyAnim.SetFloat("MoveSpeed", Mathf.Abs(movement));
+        if (hit.collider != null && isGrounded==false && playerRB.velocity.y<0)
+        {
+            if (hit.distance < 0.4)
+            {
+                Debug.Log(hit.collider.gameObject.name + " a " + hit.distance);
+                isGrounded = true;
+                MyAnim.SetBool("IsGrounded", true);
+
+            }
+            else
+            {
+                Debug.Log("Estoy en pleno salto");
+            }
+
+
+        }
+
+
+        //else
+        //{
+        //   // Saltar.SetBool("IsGrounded", true);
+        //}
+
+        playerRB.velocity = new Vector2(movimientoHorizontal * speed, playerRB.velocity.y);
+
+        //playerRB.velocity = new Vector2(movimientoHorizontal * force, movimientoVertical*force*2);
+
+
+
+        //playerRB.velocity = new Vector2(movement * force, movementVertical * force);
+
+        // float horizontalVelocity = movimiento.normalized.x * speed;
+        //  playerRB.velocity = new Vector2(horizontalVelocity*force, movementVertical * force);
+
+        //  playerRB.velocity = Vector2.right *movement* speed;
+
+        MyAnim.SetFloat("MoveSpeed", Mathf.Abs(movimientoHorizontal));
 
     }
 
